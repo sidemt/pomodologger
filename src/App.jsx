9b108cbf31dfdc99cda02d8e63afe4be7e50323d
sidemt <25644062+sidemt@users.noreply.github.com>
2021-08-30 00:15:56 +0900

    Add Google Adsense

--- src/App.jsx
+++ src/App.jsx
@@ -59,104 +59,88 @@ class App extends Component {
     };
 
     return (
-      <>
-        <div className="container">
-          <LangSelection lang={i18n.language} changeLanguage={this.changeLanguage} />
+      <div>
+        <LangSelection lang={i18n.language} changeLanguage={this.changeLanguage} />
 
-          <h1>Pomodologger</h1>
-          <p>{t('pomodologger_desc')}</p>
+        <h1>Pomodologger</h1>
+        <p>{t('pomodologger_desc')}</p>
 
-          <PomodoroClock
-            sessionLength={this.state.sessionLength}
-            onSessionLengthChange={this.handleSessionLengthChange}
-            eventName={this.state.eventName}
-            eventDetail={this.state.eventDetail}
-            calendarId={this.state.calendarId}
-          />
+        <PomodoroClock
+          sessionLength={this.state.sessionLength}
+          onSessionLengthChange={this.handleSessionLengthChange}
+          eventName={this.state.eventName}
+          eventDetail={this.state.eventDetail}
+          calendarId={this.state.calendarId}
+        />
 
-          <div className="group logs">
-            <p><strong>{t('logs')}</strong></p>
-            <p id="no-logs">{t('no_logs')}</p>
+        <div className="group logs">
+          <p><strong>{t('logs')}</strong></p>
+          <p id="no-logs">{t('no_logs')}</p>
 
-            <ol id="event-list" className="number-list" />
-          </div>
+          <ol id="event-list" className="number-list" />
+        </div>
 
-          <div className="group descriptions">
-            <div className="section">
-              <p id="authorize_desc" style={displayNone}>
-                {t('sigin_in_desc')}
-              </p>
-              <p id="authorize_failed" style={displayNone}>
-                {t('failed_to_connect_to_google')}
-              </p>
-              {/* Add buttons to initiate auth sequence and sign out */}
-              <div
-                id="authorize_button"
-                className="g-signin2"
-                style={displayNone}
-                data-longtitle="true"
-                data-theme="dark">
-              </div>
-            </div>
-
-            <div className="section">
-              <p id="authorize_success" style={displayNone}>
-                {t('log_desc')}
-              </p>
-            </div>
-
-            <div id="signed-in-only" style={displayNone}>
-              <div className="section">
-                <p><strong>{t('select_calendar')}</strong></p>
-                <select id="calendar-select" className="custom-select" name="calendarId" value={this.state.calendarId} onChange={this.handleInputChange} />
-              </div>
-
-              <div className="section">
-                <p><strong>{t('what_are_you_working_on')}</strong></p>
-                <EventForm
-                  eventName={this.state.eventName}
-                  eventDetail={this.state.eventDetail}
-                  handleInputChange={this.handleInputChange}
-                />
-              </div>
-
-              <div className="section">
-                <p>
-                  {t('try_adding_a_log')}
-                </p>
-                <button id="create_button" className="btn btn-light" onClick={this.handleCreateClick}>{t('add_a_log')}</button>
-              </div>
-
-              <div className="section">
-                <button id="signout_button" className="btn btn-secondary" style={displayNone}>{t('sign_out')}</button>
-              </div>
+        <div className="group descriptions">
+          <div className="section">
+            <p id="authorize_desc" style={displayNone}>
+              {t('sigin_in_desc')}
+            </p>
+            <p id="authorize_failed" style={displayNone}>
+              {t('failed_to_connect_to_google')}
+            </p>
+            {/* Add buttons to initiate auth sequence and sign out */}
+            <div
+              id="authorize_button"
+              className="g-signin2"
+              style={displayNone}
+              data-longtitle="true"
+              data-theme="dark">
             </div>
           </div>
 
           <div className="section">
-            <small>
-              {t('sounds_from')}:
-              {' '}
-              <a href="http://www.kurage-kosho.info" title="フリー効果音素材 くらげ工匠" target="_blank">フリー効果音素材 くらげ工匠</a>
-            </small>
+            <p id="authorize_success" style={displayNone}>
+              {t('log_desc')}
+            </p>
           </div>
 
-          <footer className="footer">
-            <ul className="justify-content-center">
-              <li className="">
-                <a className="" href="https://tools.charonworks.com/">Tools I Want</a>
-              </li>
-            </ul>
+          <div id="signed-in-only" style={displayNone}>
+            <div className="section">
+              <p><strong>{t('select_calendar')}</strong></p>
+              <select id="calendar-select" className="custom-select" name="calendarId" value={this.state.calendarId} onChange={this.handleInputChange} />
+            </div>
 
-            <small>
-              (c) 2018-2021
-              {' '}
-              <a href="https://www.charonworks.com" target="_blank">Charonworks</a>
-            </small>
-          </footer>
+            <div className="section">
+              <p><strong>{t('what_are_you_working_on')}</strong></p>
+              <EventForm
+                eventName={this.state.eventName}
+                eventDetail={this.state.eventDetail}
+                handleInputChange={this.handleInputChange}
+              />
+            </div>
 
+            <div className="section">
+              <p>
+                {t('try_adding_a_log')}
+              </p>
+              <button id="create_button" className="btn btn-light" onClick={this.handleCreateClick}>{t('add_a_log')}</button>
+            </div>
+
+            <div className="section">
+              <button id="signout_button" className="btn btn-secondary" style={displayNone}>{t('sign_out')}</button>
+            </div>
+          </div>
         </div>
-      </>
+
+        <div className="section">
+          <small>
+            {t('sounds_from')}:
+            {' '}
+            <a href="http://www.kurage-kosho.info" title="フリー効果音素材 くらげ工匠" target="_blank">フリー効果音素材 くらげ工匠</a>
+          </small>
+        </div>
+
+      </div>
     );
   }
 }
